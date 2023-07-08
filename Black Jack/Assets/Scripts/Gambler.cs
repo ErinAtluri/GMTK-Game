271790190c374ef7cfa4f2586e6737482b28cc80
NolanTaylor <46251608+NolanTaylor@users.noreply.github.com>
2023-07-07 17:11:25 -0700

    inputhandler

--- Black Jack/Assets/Scripts/Gambler.cs
+++ Black Jack/Assets/Scripts/Gambler.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Gambler : MonoBehaviour 
 {
+    Camera m_Camera;
     public Card[] hand = new Card[2];
     public int totalScore;
 
+    void OnMouseDown()
+    {
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        m_Camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Mouse mouse = Mouse.current;
+        if (mouse.leftButton.wasPressedThisFrame)
+        {
+            Vector3 mousePosition = mouse.position.ReadValue();
+            Ray ray = m_Camera.ScreenPointToRay(mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                // Use the hit variable to determine what was clicked on.
+            }
+        }
     }
 }
---
+++ Black Jack/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    private Camera _mainCamera;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+
+        var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        
+        if (!rayHit.collider) return;
+
+        Debug.Log(rayHit.collider.gameObject.name);
+    }
+}
