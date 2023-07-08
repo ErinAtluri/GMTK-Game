9a8f4bad586c9b0f6e1057f4931b046ca71dba5f
ErinAtluri <125645174+ErinAtluri@users.noreply.github.com>
2023-07-08 13:55:41 -0700

    Main Menu

---
+++ blackjack/Main Menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+
+func _on_Play_Button_pressed():
+	get_tree().change_scene("res://root.tscn")
+
+
+func _on_Credits_Button_pressed():
+	$"Credit Menu".show()
+
+
+func _on_Quit_Button_pressed():
+	get_tree().quit()
+
+
+
+func _on_Back_pressed():
+	$"Credit Menu".hide()
